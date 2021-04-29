04afc10d4e9206135c77f1f67511f26a73c8784a
ali forghani <a.forghani.y79@gmail.com>
2021-04-29 14:19:22 +0430

    Revert "ready for testing play ground"
    This reverts commit 51ed0e23ed2fd3d6ca0ccfa42d66302770504f72.

--- app/src/main/java/com/example/myapplication/MenuActivity.java
+++ app/src/main/java/com/example/myapplication/MenuActivity.java
@@ -2,9 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -12,11 +10,5 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        Button button = findViewById(R.id.letsgo);
-        button.setOnClickListener(view -> {
-            Intent intent = new Intent(getBaseContext(),MainActivity.class);
-            intent.putExtra("colorPLayerOne",0);
-            intent.putExtra("colorPLayerTwo",1);
-        });
     }
 }
