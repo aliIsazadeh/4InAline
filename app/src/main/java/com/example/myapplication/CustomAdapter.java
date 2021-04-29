ec1c233c1d21e6e411aa1534c50fe0e0636d0640
ali forghani <a.forghani.y79@gmail.com>
2021-04-29 16:17:42 +0430

    try to success

---
+++ app/src/main/java/com/example/myapplication/CustomAdapter.java
@@ -0,0 +1,55 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.media.Image;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class CustomAdapter extends ArrayAdapter<String> {
+
+    private Context context;
+
+    public CustomAdapter(@NonNull Context context, int resource ,String[] colorNames) {
+        super(context, resource,colorNames);
+        this.context=context;
+    }
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView==null){
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = layoutInflater.inflate(R.layout.spinner_item, null);
+        }
+        TextView textView = (TextView) convertView.findViewById(R.id.textItemSpinner);
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.imageItemSpinner);
+
+        textView.setText(getContext().getResources().getString(getContext().getResources().getIdentifier("__" + position, "string", getContext().getPackageName())));
+        imageView.setBackgroundColor(getContext().getResources().getColor(getContext().getResources().getIdentifier("color_" + position, "color", getContext().getPackageName())));
+
+        return convertView;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView==null){
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.spinner_item, null);
+        }
+        TextView textView = (TextView) convertView.findViewById(R.id.textItemSpinner);
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.imageItemSpinner);
+
+        textView.setText(getContext().getResources().getString(getContext().getResources().getIdentifier("__" + position, "string", getContext().getPackageName())));
+        imageView.setBackgroundColor(getContext().getResources().getColor(getContext().getResources().getIdentifier("color_" + position, "color", getContext().getPackageName())));
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/myapplication/MenuActivity.java
+++ app/src/main/java/com/example/myapplication/MenuActivity.java
@@ -4,9 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private Spinner spinnerPlayerOne;
+    private Spinner spinnerPlayerTwo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +27,42 @@ public class MenuActivity extends AppCompatActivity {
             intent.putExtra("colorPLayerTwo",1);
             startActivity(intent);
         });
+
+
+        //todo get colors name list form string.xml
+        String[] colorNames = {"red","blue","redred","yyy","u","redred","yyy","u"};
+
+
+
+        spinnerPlayerOne=findViewById(R.id.spinnerPLayerOne);
+        CustomAdapter customAdapter =  new CustomAdapter(this,R.layout.spinner_item,colorNames);
+        spinnerPlayerOne.setAdapter(customAdapter);
+        spinnerPlayerOne.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MenuActivity.this, colorNames[i], Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(MenuActivity.this, "fuck you", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        spinnerPlayerTwo=findViewById(R.id.spinnerPlayerTwo);
+        CustomAdapter customAdapter1 =  new CustomAdapter(this,R.layout.spinner_item,colorNames);
+        spinnerPlayerTwo.setAdapter(customAdapter1);
+        spinnerPlayerTwo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MenuActivity.this, colorNames[i], Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(MenuActivity.this, "fuck you", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
