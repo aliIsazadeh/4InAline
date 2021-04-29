146cb91d0387b4c831b31476b2fa0761110b652e
ali forghani <a.forghani.y79@gmail.com>
2021-04-29 16:24:34 +0430

    data can transfer into next activity

--- app/src/main/java/com/example/myapplication/MenuActivity.java
+++ app/src/main/java/com/example/myapplication/MenuActivity.java
@@ -6,62 +6,74 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MenuActivity extends AppCompatActivity {
 
     private Spinner spinnerPlayerOne;
     private Spinner spinnerPlayerTwo;
+    private int selectionOne;
+    private int selectionTwo;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        Button button = findViewById(R.id.letsgo);
-        button.setOnClickListener(view -> {
-            Intent intent = new Intent(getBaseContext(),MainActivity.class);
-            intent.putExtra("colorPLayerOne",0);
-            intent.putExtra("colorPLayerTwo",1);
-            startActivity(intent);
-        });
 
 
         //todo get colors name list form string.xml
-        String[] colorNames = {"red","blue","redred","yyy","u","redred","yyy","u"};
+        ArrayList<String> list = new ArrayList<>();
+        Field[] fields = R.string.class.getFields();
+        for (Field field : fields) {
+            list.add(field.getName());
+        }
+        String[] colorNames = (String[]) list.toArray();
 
-
-
-        spinnerPlayerOne=findViewById(R.id.spinnerPLayerOne);
-        CustomAdapter customAdapter =  new CustomAdapter(this,R.layout.spinner_item,colorNames);
+        spinnerPlayerOne = findViewById(R.id.spinnerPLayerOne);
+        CustomAdapter customAdapter = new CustomAdapter(this, R.layout.spinner_item, colorNames);
         spinnerPlayerOne.setAdapter(customAdapter);
         spinnerPlayerOne.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MenuActivity.this, colorNames[i], Toast.LENGTH_SHORT).show();
+                selectionOne = i;
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
-                Toast.makeText(MenuActivity.this, "fuck you", Toast.LENGTH_SHORT).show();
+
             }
         });
 
-        spinnerPlayerTwo=findViewById(R.id.spinnerPlayerTwo);
-        CustomAdapter customAdapter1 =  new CustomAdapter(this,R.layout.spinner_item,colorNames);
+        spinnerPlayerTwo = findViewById(R.id.spinnerPlayerTwo);
+        CustomAdapter customAdapter1 = new CustomAdapter(this, R.layout.spinner_item, colorNames);
         spinnerPlayerTwo.setAdapter(customAdapter1);
         spinnerPlayerTwo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MenuActivity.this, colorNames[i], Toast.LENGTH_SHORT).show();
+                selectionTwo = i;
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
-                Toast.makeText(MenuActivity.this, "fuck you", Toast.LENGTH_SHORT).show();
+
             }
+        });
+
+
+        Button button = findViewById(R.id.letsgo);
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+            intent.putExtra("colorPLayerOne", selectionOne);
+            intent.putExtra("colorPLayerTwo", selectionTwo);
+            startActivity(intent);
         });
 
     }
